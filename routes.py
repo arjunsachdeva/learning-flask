5846d7b320f875381e73d54cb3df963c62aff6bc
Arjun Sachdeva <asachdeva@gogoair.com>
2018-07-04 11:54:38 +0530

    Fully running project

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-from models import db
-from forms import SignupForm
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
@@ -23,10 +23,50 @@ def signup():
     if request.method == 'POST':
         if form.validate() == False:
             return render_template("signup.html", form=form)
+        else:
+            newUser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newUser)
+            db.session.commit()
+            
+            session['email'] = newUser.email
+            return (url_for('home'))
         return "Success"
     elif request.method == 'GET':
         return render_template("signup.html", form=form)
 
+@app.route("/home")
+def home():
+    #protect the pages
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    return render_template("home.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template("login.html", form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+           
+            #Below line is querying the database. 
+            user = User.query.filter_by(email=email).first()
+            if user is not None and user.check_password(password):
+                session['email'] = form.email.data
+                return redirect(url_for('home'))
+            else:
+                return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
   app.run(debug=True)
